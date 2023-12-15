3334c8661a1b449ba9e8cad5ac441da398c26b4d
kylaerod <kylarodriguez52@gmail.com>
2023-12-14 20:12:19 -0800

    audiomack impementation

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,56 +1,6 @@
-// Testing React Feature
-
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// import {Container, InputGroup, FormControl, Button, Row, Card} from 'react-bootstrap'
-// import { useState, useEffect} from 'react-bootstrap'
-
-// function App () {
-//     cont [ searchInput, setSearchInput] = useState("");
-
-//     return (
-//         <div className="app">
-//             <Container>
-//                 <InputGroup className="mb-3" size="lg">
-//                     <FormControl
-//                         placeholder = "Search For Artist"
-//                         type='input'
-//                         onKeyPress= {event=> {
-//                             if (event.key == "Enter") {
-//                                 console.log("Pressed enter")
-//                             }
-//                         }}
-//                         onChange={event => setSearchInput(event.target.value)}
-//                     />
-//                     <Button onClick={event => {console.log("clicked Button")}}>
-//                         Search
-//                     </Button>
-//                 </InputGroup>
-//             </Container>
-//             <Container>
-//                 <Card>
-//                    <Card.Img src='#' />
-//                    <Card.Body>
-//                         <Card.Title>Album Name Here</Card.Title>
-//                    </Card.Body>
-//                 </Card>
-//             </Container>
-//         </div>
-//     )
-
-// const searchData = [
-//     "Artist 1",
-//     "Song 1",
-//     "Album 1",
-//     // Can add more items as needed
-// ];
-
-// function performSearch() {
-//     const searchTerm = document.getElementById("searchInput").value.toLowerCase();
-//     const results = searchData.filter(item => item.toLowerCase().includes(searchTerm));
-
-//     // To customize the display of the search results
-//     alert(results.length > 0 ? `Found: ${results.join(", ")}` : "No results found.");
-// }
+const keywordList=document.querySelector(".keyword-list")
+const searchBtn= document.getElementById("search-btn")
+const inputEL= document.querySelector(".input")
 
 let availableKeywords = [
     'Tyler the Creator',
@@ -66,6 +16,18 @@ let availableKeywords = [
     'Michael Jackson',
     'Kanye West',
 ];
+
+// for (let i = 0; i < availableKeywords.length; i++) {
+//    const btn=document.createElement("button")
+//    btn.textContent=availableKeywords[i]
+//     keywordList.append(btn)
+// }
+
+// keywordList.addEventListener("click", (e)=>{
+//     e.preventDefault()
+//     const artist= this.event.target.textContent
+//     console.log(artist);
+// })
 
 const resultsBox = document.querySelector(".search-results")
 const inputBox = document.querySelector(".input")
@@ -94,45 +56,66 @@ function display (result){
 
 // Spotify API
 
-// var clientId = '851808986c0a46ec8232b53e07dfb96e';
-// var clientSecret = '73dfd0082e9b45d3a4f35128439e180c';
-// var redirectUri = 'http://127.0.0.1:5500';
-// var accessToken = window.location.hash.substring(1).split('&')[0].split('=')[1];
-// localStorage.setItem('access_token', accessToken);
+var clientId = '851808986c0a46ec8232b53e07dfb96e';
+var clientSecret = '73dfd0082e9b45d3a4f35128439e180c';
+var redirectUri = 'http://127.0.0.1:5500';
+var accessToken = window.location.hash.substring(1).split('&')[0].split('=')[1];
+localStorage.setItem('access_token', accessToken);
 
 
-// var trackId = '3qQbCzHBycnDpGskqOWY0E';
+var trackId = '3qQbCzHBycnDpGskqOWY0E';
 
-// async function getProfile() {
-//     let accessToken = localStorage.getItem('access_token');
+async function getProfile() {
+    let accessToken = localStorage.getItem('access_token');
 
-//     const response = await fetch('https://api.spotify.com/v1/me', {
-//         headers: {
-//             Authorization: 'Bearer ' + accessToken
-//         }
-//     });
+    const response = await fetch('https://api.spotify.com/v1/me', {
+        headers: {
+            Authorization: 'Bearer ' + accessToken
+        }
+    });
 
-//     const data = await response.json();
-//     console.log('User Profile:', data);
-// }
+    const data = await response.json();
+    console.log('User Profile:', data);
+}
 
-// // Call the getProfile function
-// getProfile();
+// Call the getProfile function
+getProfile();
 
-// // Fetch track information
-// fetch(`https://api.spotify.com/v1/tracks/${trackId}`, {
-//     method: 'get',
-//     headers: {
-//         'Authorization': `Bearer ${accessToken}`,
-//     },
-// })
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log('Track Information:', data);
-//     })
-//     .catch(error => {
-//         console.error('Error fetching track data from Spotify', error);
-//     });
+// Fetch track information
+fetch(`https://api.spotify.com/v1/tracks/${trackId}`, {
+    method: 'get',
+    headers: {
+        'Authorization': `Bearer ${accessToken}`,
+    },
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log('Track Information:', data);
+    })
+    .catch(error => {
+        console.error('Error fetching track data from Spotify', error);
+    });
+
+    // Audiomack results
+
+    function getApi(song){
+        const url=`https://api.audiomack.com/v1/search?q=${song}`
+        fetch(url)
+        .then((response)=>{
+            return response.json()
+        })
+        .then((data)=>{
+            console.log(data);
+        })
+    }
+
+    searchBtn.addEventListener("click", ()=>{
+        const searchSong=inputEL.value
+        console.log(searchSong);
+        getApi(searchSong)
+    })
+
+    
 
 //get track/id
 
