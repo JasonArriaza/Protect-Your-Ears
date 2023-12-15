5e0897624020a2dfcd4c1f8c7ef6232d026f3838
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2023-12-14 22:24:31 -0500

    added new images and updated marquee section

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -37,102 +37,61 @@
 //         </div>
 //     )
 
-// const searchData = [
-//     "Artist 1",
-//     "Song 1",
-//     "Album 1",
-//     // Can add more items as needed
-// ];
-
-// function performSearch() {
-//     const searchTerm = document.getElementById("searchInput").value.toLowerCase();
-//     const results = searchData.filter(item => item.toLowerCase().includes(searchTerm));
-
-//     // To customize the display of the search results
-//     alert(results.length > 0 ? `Found: ${results.join(", ")}` : "No results found.");
-// }
-
-let availableKeywords = [
-    'Tyler the Creator',
-    'Wu-Tang Clan',
-    'Outkast',
-    'Drake',
-    'Childish Gambino',
-    'Future',
-    'J. Cole',
-    'Kendrick Lamar',
-    'Eminem',
-    'Griselda',
-    'Michael Jackson',
-    'Kanye West',
+const searchData = [
+    "Artist 1",
+    "Song 1",
+    "Album 1",
+    // Can add more items as needed
 ];
 
-const resultsBox = document.querySelector(".search-results")
-const inputBox = document.querySelector(".input")
+function performSearch() {
+    const searchTerm = document.getElementById("searchInput").value.toLowerCase();
+    const results = searchData.filter(item => item.toLowerCase().includes(searchTerm));
 
-inputBox.onkeyup = function(){
-    let result = [];
-    let input = inputBox.value;
-    if(input.length){
-        result = availableKeywords.filter((keyword)=>{
-            return keyword.toLowerCase().includes(input.toLowerCase());
-        });
-
-    console.log(result);
-
-    }
-    display(result);
-}
-
-function display (result){
-    const content = result.map((list)=>{
-        return "<li>" + list + "</li>";
-    });
-
-    resultsBox.innerHTML = "<ul>" + content.join("") + "</ul>"
+    // To customize the display of the search results
+    alert(results.length > 0 ? `Found: ${results.join(", ")}` : "No results found.");
 }
 
 // Spotify API
+var clientId = '851808986c0a46ec8232b53e07dfb96e';
+var clientSecret = '73dfd0082e9b45d3a4f35128439e180c';
+var redirectUri = 'http://127.0.0.1:5500';
+var accessToken = window.location.hash.substring(1).split('&')[0].split('=')[1];
+localStorage.setItem('access_token', accessToken);
 
-// var clientId = '851808986c0a46ec8232b53e07dfb96e';
-// var clientSecret = '73dfd0082e9b45d3a4f35128439e180c';
-// var redirectUri = 'http://127.0.0.1:5500';
-// var accessToken = window.location.hash.substring(1).split('&')[0].split('=')[1];
-// localStorage.setItem('access_token', accessToken);
 
+var trackId = '3qQbCzHBycnDpGskqOWY0E';
 
-// var trackId = '3qQbCzHBycnDpGskqOWY0E';
+async function getProfile() {
+    let accessToken = localStorage.getItem('access_token');
 
-// async function getProfile() {
-//     let accessToken = localStorage.getItem('access_token');
+    const response = await fetch('https://api.spotify.com/v1/me', {
+        headers: {
+            Authorization: 'Bearer ' + accessToken
+        }
+    });
 
-//     const response = await fetch('https://api.spotify.com/v1/me', {
-//         headers: {
-//             Authorization: 'Bearer ' + accessToken
-//         }
-//     });
+    const data = await response.json();
+    console.log('User Profile:', data);
+}
 
-//     const data = await response.json();
-//     console.log('User Profile:', data);
-// }
+// Call the getProfile function
+getProfile();
 
-// // Call the getProfile function
-// getProfile();
-
-// // Fetch track information
-// fetch(`https://api.spotify.com/v1/tracks/${trackId}`, {
-//     method: 'get',
-//     headers: {
-//         'Authorization': `Bearer ${accessToken}`,
-//     },
-// })
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log('Track Information:', data);
-//     })
-//     .catch(error => {
-//         console.error('Error fetching track data from Spotify', error);
-//     });
+// Fetch track information
+fetch(`https://api.spotify.com/v1/tracks/${trackId}`, {
+    method: 'get',
+    headers: {
+        'Authorization': `Bearer ${accessToken}`,
+    },
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log('Track Information:', data);
+    })
+    .catch(error => {
+        console.error('Error fetching track data from Spotify', error);
+    });
 
 //get track/id
 
